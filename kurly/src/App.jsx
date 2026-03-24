c80c1bfa4244ba84dcbce3199835681cc7900843
hyrionbs <gorabbit2001@naver.com>
2026-03-24 20:45:51 +0900

    restart

---
+++ kurly/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import Home from './pages/home'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>hi</div>
+      <Home />
+    </>
+  )
+}
+
+export default App
---
+++ kurly/src/pages/home.jsx
@@ -0,0 +1,5 @@
+function Home() {
+  return <h1>홈페이지</h1>;
+}
+
+export default Home;
