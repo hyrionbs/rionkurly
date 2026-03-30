b1572b2da6416341fa6f2d83e1e16c1326a502d2
노트북 <your_email@example.com>
2026-03-30 12:55:51 +0900

    login창 완성

--- kurly/src/pages/login.jsx
+++ kurly/src/pages/login.jsx
@@ -7,26 +7,30 @@ function Login() {
       <h1>Header</h1>
     </div>
   <div class="login">
-    <p class="loginText">로그인</p>
+    <div>
+      <p class="loginText">로그인</p>
+    </div>
 
     <div>
       <div>
-        <div>
-          <input class="loginInput" type="text" placeholder="아이디를 입력해주세요" />
-          <input class="loginInput" type="password" placeholder="비밀번호를 입력해주세요" />
+        <div className="loginInputDiv">
+          <input className="loginInput" type="text" placeholder="아이디를 입력해주세요" />
+          <input className="loginInput" type="password" placeholder="비밀번호를 입력해주세요" />
         </div>
-        <div>
+        <div className="loginInputSc">
           <a href="">아이디 찾기</a>
           <a href="">비밀번호 찾기</a>
         </div>
-        <div>
+        <div className="loSiBtn">
           <button class="loginBtn">로그인</button>
           <button class="signBtn">회원가입</button>
         </div>
         <div>
           <p class="loginText">로그인</p>
-          <button class="loginBtn">네이버로 계산하기</button>
-          <button class="signBtn">카카오로 계속하기</button>
+          <div class="NKlogin">
+            <button class="NaverloginBtn">네이버로 계산하기</button>
+            <button class="KakaologinBtn">카카오로 계속하기</button>
+          </div>
         </div>
       </div>
     </div>
