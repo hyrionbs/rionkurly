19d8941a81252ea8955e2ea1f87fab7e259db27b
노트북 <your_email@example.com>
2026-04-12 08:17:31 +0900

    logintest

--- kurly/src/pages/login.jsx
+++ kurly/src/pages/login.jsx
@@ -3,12 +3,12 @@ import "./login.css";
 function Login() {
   return (
   <div> 
-    <div class ="loginHD">
+    <div className ="loginHD">
       <h1>Header</h1>
     </div>
-  <div class="loginPage">
+  <div className="loginPage">
     <div>
-      <p class="loginText">로그인</p>
+      <p className="loginText">로그인</p>
     </div>
 
     <div>
@@ -22,14 +22,14 @@ function Login() {
           <a href="">비밀번호 찾기</a>
         </div>
         <div className="loSiBtn">
-          <button class="loginBtn">로그인</button>
-          <button class="signBtn">회원가입</button>
+          <button className="loginBtn">로그인</button>
+          <button className="signBtn">회원가입</button>
         </div>
         <div>
-          <p class="loginText">간편 로그인</p>
-          <div class="NKlogin">
-            <button class="NaverloginBtn">네이버로 계산하기</button>
-            <button class="KakaologinBtn">카카오로 계속하기</button>
+          <p className="loginText">간편 로그인</p>
+          <div className="NKlogin">
+            <button className="NaverloginBtn">네이버로 계산하기</button>
+            <button className="KakaologinBtn">카카오로 계속하기</button>
           </div>
         </div>
       </div>
--- kurly/src/pages/signup.jsx
+++ kurly/src/pages/signup.jsx
@@ -2,71 +2,73 @@ import "./signup.css";
 
 
 function signup() {
-  return 
+  return (
   <div>
-      dkssud
-      <div>
+      <div className="signupTitle">
         <p>회원가입</p>
       </div>
-      <div className="aa">
-        <span>*</span>회원가입
+      <div className="signupBox" >
+        <div className="aa">
+          <span id="starIcon">*</span> 필수입력사항
+        </div>
+
+        <div className="signupInnerbox">
+          <div className="signupInner">
+            <div className="signupInnerTitle">
+              아이디<span id="starIcon">*</span>
+            </div>
+            <div className="signupInnerInput">
+              <input type="text" placeholder="아이디를 입력해주세요." />
+            </div>
+            <div className="signupInnerEmpty"></div>
+          </div>
+
+          <div className="signupInner">
+            <div className="signupInnerTitle">
+              비밀번호<span id="starIcon">*</span>
+            </div>
+            <div className="signupInnerInput">
+              <input type="password" placeholder="비밀번호를 입력해주세요." />
+            </div>
+            <div className="signupInnerEmpty"></div>
+          </div>
+
+          <div className="signupInner">
+            <div className="signupInnerTitle">
+              비밀번호 확인<span id="starIcon">*</span>
+            </div>
+            <div className="signupInnerInput">
+              <input type="password" placeholder="비밀번호를 한번 더 입력해주세요." />
+            </div>
+            <div className="signupInnerEmpty"></div>
+          </div>
+
+          <div className="signupInner">
+            <div className="signupInnerTitle">
+              이름<span id="starIcon">*</span>
+            </div>
+            <div>
+              <input type="text" placeholder="이름을 입력해주세요." />
+            </div>
+            <div className="signupInnerEmpty"></div>
+          </div>
+
+          <div className="signupInner">
+            <div className="signupInnerTitle">
+              이메일<span id="starIcon">*</span>
+            </div>
+            <div>
+              <input type="email" placeholder="아이디를 입력해주세요." />
+            </div>
+            <div className="signupInnerEmpty"></div>
+          </div>
+        </div>
       </div>
 
       <div>
-        <div>
-          아이디<span>*</span>
-        </div>
-        <div>
-          <input type="text" placeholder="아이디를 입력해주세요." />
-        </div>
         <div></div>
       </div>
-
-      <div>
-        <div>
-          비밀번호<span>*</span>
-        </div>
-        <div>
-          <input type="password" placeholder="비밀번호를 입력해주세요." />
-        </div>
-        <div></div>
-      </div>
-
-      <div>
-        <div>
-          비밀번호 확인<span>*</span>
-        </div>
-        <div>
-          <input type="password" placeholder="비밀번호를 한번 더 입력해주세요." />
-        </div>
-        <div></div>
-      </div>
-
-      <div>
-        <div>
-          이름<span>*</span>
-        </div>
-        <div>
-          <input type="text" placeholder="이름을 입력해주세요." />
-        </div>
-        <div></div>
-      </div>
-
-      <div>
-        <div>
-          이메일<span>*</span>
-        </div>
-        <div>
-          <input type="email" placeholder="아이디를 입력해주세요." />
-        </div>
-        <div></div>
-      </div>
-
-
-      <div>
-        <div></div>
-      </div>
-  </div>
+  </div>);
 }
 
 export default signup;
